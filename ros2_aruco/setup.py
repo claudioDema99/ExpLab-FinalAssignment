a6f6868e42c5cec246ffa2ea71c0da47157e968d
Gianluca Galvagni <gianluca.galvagni99@gmail.com>
2024-01-09 11:53:14 +0100

    lol

--- ros2_aruco/setup.py
+++ ros2_aruco/setup.py
@@ -1,6 +1,4 @@
 from setuptools import setup
-import os
-from glob import glob
 
 package_name = 'ros2_aruco'
 
@@ -12,8 +10,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
